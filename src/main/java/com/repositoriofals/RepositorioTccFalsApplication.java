f8e3287b5b5238e3ed46909492af7f5dcf36dc66
israel gomes <rael_goomes@hotmail.com>
2019-04-14 16:23:44 -0300

    Repositorio Tcc fals - projeto criado java/sprintboot

---
+++ src/main/java/com/repositoriofals/RepositorioTccFalsApplication.java
@@ -0,0 +1,13 @@
+package com.repositoriofals;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RepositorioTccFalsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RepositorioTccFalsApplication.class, args);
+	}
+
+}
